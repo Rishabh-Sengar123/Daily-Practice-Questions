9b51e95a8ff11c2e36bafb4e9e9436481028b4c9
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-03 10:43:32 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,40 @@
+// Last updated: 8/3/2025, 10:43:32 AM
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid; 
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1; 
+            }
+        }
+
+        
+        return left;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        
+        int[] nums1 = {1, 3, 5, 6};
+        int target1 = 5;
+        System.out.println("Output: " + solution.searchInsert(nums1, target1)); 
+
+        
+        int[] nums2 = {1, 3, 5, 6};
+        int target2 = 2;
+        System.out.println("Output: " + solution.searchInsert(nums2, target2)); 
+
+        
+        int[] nums3 = {1, 3, 5, 6};
+        int target3 = 7;
+        System.out.println("Output: " + solution.searchInsert(nums3, target3)); 
+    }
+}
