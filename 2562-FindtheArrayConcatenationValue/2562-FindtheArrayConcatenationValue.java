0bc77cea2b14befbd104a3a24ce52e13fb1d6ddf
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-03 11:15:44 +0530

    Create 2562-FindtheArrayConcatenationValue

---
+++ 2562-FindtheArrayConcatenationValue/2562-FindtheArrayConcatenationValue.java
@@ -0,0 +1,20 @@
+// Last updated: 8/3/2025, 11:15:44 AM
+class Solution {
+    public long findTheArrayConcVal(int[] nums) {
+        int l = 0;
+        int r = nums.length-1;
+        long sum = 0;
+        while(l<=r){
+            if(l==r){
+                sum+= nums[l];
+            }else{
+                String a = nums[l]+""+nums[r];
+                sum+= Integer.parseInt(a);
+                
+            }
+            l++;
+            r--;
+        }
+        return sum;
+    }
+}
