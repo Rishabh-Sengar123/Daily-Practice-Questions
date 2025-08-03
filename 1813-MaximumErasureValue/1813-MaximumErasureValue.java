1ee9607ce0dd44fdb86b307827a18d1187424bf0
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-03 09:53:36 +0530

    Create 1813-MaximumErasureValue

---
+++ 1813-MaximumErasureValue/1813-MaximumErasureValue.java
@@ -0,0 +1,25 @@
+// Last updated: 8/3/2025, 9:53:36 AM
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        int left = 0, right = 0;
+        int currentSum = 0, maxSum = 0;
+
+        while (right < nums.length) {
+            while (seen.contains(nums[right])) {
+                seen.remove(nums[left]);
+                currentSum -= nums[left];
+                left++;
+            }
+            seen.add(nums[right]);
+            currentSum += nums[right];
+            maxSum = Math.max(maxSum, currentSum);
+            right++;
+        }
+
+        return maxSum;
+    }
+}
