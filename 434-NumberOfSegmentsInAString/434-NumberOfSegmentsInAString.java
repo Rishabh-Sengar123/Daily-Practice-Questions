09a499653d149f341ffddbcc415971f9c474f5c5
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-03 09:54:27 +0530

    Create 434-NumberOfSegmentsInAString

---
+++ 434-NumberOfSegmentsInAString/434-NumberOfSegmentsInAString.java
@@ -0,0 +1,8 @@
+// Last updated: 8/3/2025, 9:54:27 AM
+import java.util.*;
+class Solution {
+    public int countSegments(String s) {
+        StringTokenizer st = new StringTokenizer(s);
+        return st.countTokens();
+    }
+}
