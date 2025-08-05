a1e05ebf002125ccb396082215f626a902bcb99d
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-05 21:55:46 +0530

    Create 940-FruitIntoBaskets

---
+++ 940-FruitIntoBaskets/940-FruitIntoBaskets.java
@@ -0,0 +1,25 @@
+// Last updated: 8/5/2025, 9:55:45 PM
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> basket = new HashMap<>();
+        int left = 0, maxFruits = 0;
+
+        for (int right = 0; right < fruits.length; right++) {
+            int fruit = fruits[right];
+            basket.put(fruit, basket.getOrDefault(fruit, 0) + 1);
+
+            while (basket.size() > 2) {
+                int leftFruit = fruits[left];
+                basket.put(leftFruit, basket.get(leftFruit) - 1);
+                if (basket.get(leftFruit) == 0) {
+                    basket.remove(leftFruit);
+                }
+                left++;
+            }
+
+            maxFruits = Math.max(maxFruits, right - left + 1);
+        }
+
+        return maxFruits;
+    }
+}
