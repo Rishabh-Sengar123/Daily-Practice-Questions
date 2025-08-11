158f88ec1a823ca4f53d48392cb6ecfea5aa2179
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-11 23:53:45 +0530

    Create 3471-MinimumAverageOfSmallestAndLargestElements

---
+++ 3471-MinimumAverageOfSmallestAndLargestElements/3471-MinimumAverageOfSmallestAndLargestElements.java
@@ -0,0 +1,18 @@
+// Last updated: 8/11/2025, 11:53:44 PM
+import java.util.*;
+
+class Solution {
+    public double minimumAverage(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        double average = Double.MAX_VALUE;
+        while(left < right){
+            double min = (nums[left]+nums[right])/2.0;
+            average = Math.min(average, min);
+            left++;
+            right--;
+        }
+        return average;
+    }
+}
