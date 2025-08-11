e140b76bf639bcc4462b7c6cdf38077e5db74c25
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-11 22:48:23 +0530

    File updated at 8/11/2025, 10:48:22 PM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
@@ -1,15 +1,22 @@
-// Last updated: 8/11/2025, 10:36:22 PM
+// Last updated: 8/11/2025, 10:48:22 PM
 class Solution {
-    public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        int l = 0;
-        int r = nums.length-1;
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i+1]==nums[i]){
-                return nums[i];
+    public String reverseOnlyLetters(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            if(!Character.isLetter(arr[left])){
+                left++;
+            }else if(!Character.isLetter(arr[right])){
+                right--;
+            }else{
+                char temp = arr[right];
+                arr[right] = arr[left];
+                arr[left] = temp;
+                left++;
+                right--;
             }
         }
-        return 1;
-
+        return new String(arr);
     }
 }
