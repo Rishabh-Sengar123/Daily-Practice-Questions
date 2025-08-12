de422afae1e45ffa77b9a25012592a2aeae92631
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-12 20:00:15 +0530

    Create 2787-WaystoExpressanIntegerasSumofPowers

---
+++ 2787-WaystoExpressanIntegerasSumofPowers/2787-WaystoExpressanIntegerasSumofPowers.java
@@ -0,0 +1,36 @@
+// Last updated: 8/12/2025, 8:00:15 PM
+import java.util.*;
+
+public class Solution {
+    static final int MOD = 1_000_000_007;
+
+    public int numberOfWays(int n, int x) {
+        List<Integer> powersList = new ArrayList<>();
+        int base = 1;
+        while (true) {
+            long val = (long) Math.pow(base, x);
+            if (val > n) break;
+            powersList.add((int) val);
+            base++;
+        }
+
+        int m = powersList.size();
+        int[] powers = new int[m];
+        for (int i = 0; i < m; i++) powers[i] = powersList.get(i);
+
+        // dp[i][t] means number of ways using first i powers to form sum t
+        // We'll optimize space to dp[t] only (rolling array)
+        int[] dp = new int[n + 1];
+        dp[0] = 1;  // one way to form sum 0: empty set
+
+        for (int i = 0; i < m; i++) {
+            int power = powers[i];
+            // go backwards to avoid reuse of same power
+            for (int t = n; t >= power; t--) {
+                dp[t] = (dp[t] + dp[t - power]) % MOD;
+            }
+        }
+
+        return dp[n];
+    }
+}
