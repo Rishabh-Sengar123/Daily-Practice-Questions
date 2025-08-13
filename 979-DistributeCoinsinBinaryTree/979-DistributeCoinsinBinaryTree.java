71ff97dc741f66f114fec416874428c3d7734deb
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-13 11:45:24 +0530

    Create 979-DistributeCoinsinBinaryTree

---
+++ 979-DistributeCoinsinBinaryTree/979-DistributeCoinsinBinaryTree.java
@@ -0,0 +1,50 @@
+// Last updated: 8/13/2025, 11:45:23 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int moves = 0;
+    
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return moves;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+
+        moves += Math.abs(left) + Math.abs(right);
+
+        // Return excess coins to parent
+        return node.val + left + right - 1;
+    }
+}
+
+// Definition for TreeNode:
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
