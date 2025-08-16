4ed881884ee54343b85017cc4d8af090996e9cee
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-16 12:55:25 +0530

    File updated at 8/16/2025, 12:55:24 PM

--- 1323-Maximum69Number/1323-Maximum69Number.java
+++ 1323-Maximum69Number/1323-Maximum69Number.java
@@ -1,16 +1,14 @@
-// Last updated: 8/16/2025, 12:51:52 PM
+// Last updated: 8/16/2025, 12:55:24 PM
 class Solution {
     public int maximum69Number (int num) {
        
-        char[] digits = String.valueOf(num).toCharArray();
-   
-        for (int i = 0; i < digits.length; i++) {
-            if (digits[i] == '6') {
-                digits[i] = '9';
-                break; 
+        char[] set = String.valueOf(num).toCharArray();
+        for(int i=0; i<set.length; i++){
+            if(set[i]=='6'){
+                set[i] = '9';
+                break;
             }
         }
-     
-        return Integer.parseInt(new String(digits));
+        return Integer.parseInt(new String(set));
     }
 }
