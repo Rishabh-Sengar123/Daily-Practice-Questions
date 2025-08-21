8210577c7655124b2e03432d880437659ab43232
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-21 10:50:29 +0530

    Create 1628-CountSubmatricesWithAllOnes

---
+++ 1628-CountSubmatricesWithAllOnes/1628-CountSubmatricesWithAllOnes.java
@@ -0,0 +1,30 @@
+// Last updated: 8/21/2025, 10:50:27 AM
+public class Solution {
+    public int numSubmat(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[][] dp = new int[m][n];
+        int res = 0;
+        
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    dp[i][j] = (j == 0 ? 1 : dp[i][j - 1] + 1);
+                }
+            }
+        }
+        
+       
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < m; ++i) {
+                if (mat[i][j] == 0) continue;
+                int minWidth = dp[i][j];
+                for (int k = i; k >= 0 && minWidth > 0; k--) {
+                    minWidth = Math.min(minWidth, dp[k][j]);
+                    res += minWidth;
+                }
+            }
+        }
+        return res;
+    }
+}
