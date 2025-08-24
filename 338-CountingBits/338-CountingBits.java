01e9944389cb9f3b56abbc9eb1e6901f032eeacb
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-24 18:36:56 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.java
@@ -0,0 +1,10 @@
+// Last updated: 8/24/2025, 6:36:55 PM
+class Solution {
+    public int[] countBits(int n) {
+        int[] arr = new int[n+1];
+        for(int i=0; i<n+1; i++){
+            arr[i] = Integer.bitCount(i);
+        }
+        return arr;
+    }
+}
