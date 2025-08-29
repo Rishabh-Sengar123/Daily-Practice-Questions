73b85b8c95de5dc64be8cf9daaaf660a0f25c4e9
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-08-29 20:00:51 +0530

    File updated at 8/29/2025, 8:00:50 PM

--- 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
@@ -1,35 +1,29 @@
-// Last updated: 8/3/2025, 9:55:15 AM
-import java.util.*;
+// Last updated: 8/29/2025, 8:00:50 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> nn = new ArrayList<>();
+        if(root == null){
+            return nn;
+        }
+        nn.addAll(inorderTraversal(root.left));
+        nn.add(root.val);
+        nn.addAll(inorderTraversal(root.right));
 
-class TreeNode {
-    int val;
-    TreeNode left, right;
-    TreeNode(int val) {
-        this.val = val;
-    }
-}
-
-public class Solution {
-    public static void main(String[] args) {
-        // Creating the tree: [1,null,2,3]
-        TreeNode root = new TreeNode(1);
-        root.right = new TreeNode(2);
-        root.right.left = new TreeNode(3);
-
-        List<Integer> result = inorderTraversal(root);
-        System.out.println(result);  // Output: [1, 3, 2]
-    }
-
-    public static List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        inorderHelper(root, result);
-        return result;
-    }
-
-    public static void inorderHelper(TreeNode node, List<Integer> result) {
-        if (node == null) return;
-        inorderHelper(node.left, result);
-        result.add(node.val);
-        inorderHelper(node.right, result);
+        return nn;
     }
 }
