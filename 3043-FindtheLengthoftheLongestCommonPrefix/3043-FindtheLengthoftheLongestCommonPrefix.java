60eeab82879cccd9bdb8c4fdfe9827e1a80c95a2
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-09-01 20:58:55 +0530

    File updated at 9/1/2025, 8:58:54 PM

--- 3043-FindtheLengthoftheLongestCommonPrefix/3043-FindtheLengthoftheLongestCommonPrefix.java
+++ 3043-FindtheLengthoftheLongestCommonPrefix/3043-FindtheLengthoftheLongestCommonPrefix.java
@@ -1,13 +1,5 @@
-// Last updated: 9/1/2025, 8:56:52 PM
+// Last updated: 9/1/2025, 8:58:54 PM
 class Solution {
-    private int digitCount(int num) {
-        int len = 0;
-        while (num > 0) {
-            len++;
-            num /= 10;
-        }
-        return len;
-    }
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
         HashSet<Integer> set = new HashSet<>();
         for(int num : arr1){
@@ -22,7 +14,7 @@ class Solution {
             int temp = num;
             while(temp>0){
                 if(set.contains(temp)){
-                    maxlen = Math.max(maxlen, digitCount(temp));
+                    maxlen = Math.max(maxlen,(int) Math.log10(temp) + 1);
                 }
                 temp/=10;
             }
