985a00c1c92f11b5d4924e1876bec2a821b56e46
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-09-27 00:31:43 +0530

    Create 605-CanPlaceFlowers

---
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.java
@@ -0,0 +1,39 @@
+// Last updated: 9/27/2025, 12:31:42 AM
+class Solution {
+    public boolean canPlaceFlowers(int[] nums, int n) {
+        boolean ans = false;
+        for(int i=0; i<nums.length ;i++){
+            if(nums[i]==0){
+                boolean left = (i==0 || nums[i-1]==0);
+                boolean right = (i==nums.length-1 || nums[i+1]==0);
+                if(left && right){
+                    nums[i] = 1;
+                    n--;
+                }
+            }
+
+        }
+        return n <= 0; 
+    }
+
+
+}
+
+
+// class Solution {
+//     public boolean canPlaceFlowers(int[] nums, int n) {
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] == 0) {
+//                 boolean left = (i == 0 || nums[i - 1] == 0);
+//                 boolean right = (i == nums.length - 1 || nums[i + 1] == 0);
+//                 if (left && right) {
+//                     nums[i] = 1;
+//                     n--;
+//                 }
+//             }
+//         }
+
+//         return n <= 0;  
+//     }
+
+// }
