57f73048140191cf56923a6f6abc7538089beb99
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-09-26 14:45:55 +0530

    File updated at 9/26/2025, 2:45:54 PM

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.java
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.java
@@ -1,11 +1,36 @@
-// Last updated: 9/26/2025, 2:15:21 PM
+// Last updated: 9/26/2025, 2:45:54 PM
 class Solution {
-    public int search(int[] nums, int target) {
-        for(int i=0; i<nums.length ; i++){
-            if(nums[i] == target){
-                return i;
-            }
-        }
-        return -1;
+    public int countPrimes(int n) {
+    if(n<=2)
+    {
+        return 0;
     }
+    return PrimeSieve(n);
+    }
+    public static int PrimeSieve(int n) {
+	int [] ans=new int [n];
+	ans[0]=ans[1]=1;
+	for(int i=2;i*i<=ans.length;i++) {
+		if(ans[i]==0) // i prime hai
+		{
+			
+			for(int j=2;i*j <ans.length;j++)
+				//2*2
+			{
+				ans[i*j]=1;
+			}
+		}
+			
+	}
+	int c=0;
+	for(int i=2;i<ans.length;i++)
+	{
+		if(ans[i]==0)
+		{
+			c++;
+		}
+	}
+	return c;
+	
+}
 }
