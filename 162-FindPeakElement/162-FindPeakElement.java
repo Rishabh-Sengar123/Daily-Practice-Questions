67474ea9969b80a4d374ea26b7b27348d605f4b9
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-09-27 13:12:12 +0530

    Create 162-FindPeakElement

---
+++ 162-FindPeakElement/162-FindPeakElement.java
@@ -0,0 +1,13 @@
+// Last updated: 9/27/2025, 1:12:11 PM
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max = Arrays.stream(nums).max().getAsInt();
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]== max){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
