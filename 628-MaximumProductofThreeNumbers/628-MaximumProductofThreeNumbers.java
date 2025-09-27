6fee905296b9f44b63c2befd0694a821f52557cd
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-09-27 13:37:49 +0530

    File updated at 9/27/2025, 1:37:49 PM

--- 628-MaximumProductofThreeNumbers/628-MaximumProductofThreeNumbers.java
+++ 628-MaximumProductofThreeNumbers/628-MaximumProductofThreeNumbers.java
@@ -1,17 +1,19 @@
-// Last updated: 9/27/2025, 1:28:31 PM
+// Last updated: 9/27/2025, 1:37:49 PM
 class Solution {
-    public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        int product = 1;
-        int ans = Integer.MIN_VALUE;
-        int hello = nums[0]*nums[1]*nums[nums.length-1];
-        for(int i=0; i<nums.length - 2; i++){
-            product = nums[i]*nums[i+1]*nums[i+2];
-            ans = Math.max(ans , product);
-            ans = Math.max(ans , hello);
-
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int mid = 0;
+        while(l<=r){
+            mid = l + (r-l)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid]<target){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
         }
-        return ans;
+        return -1;
     }
-
 }
