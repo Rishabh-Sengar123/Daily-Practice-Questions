b35b084d6abb18bf1dfeab2a6707956483ddf808
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-09-29 13:11:28 +0530

    Create 1672-RichestCustomerWealth

---
+++ 1672-RichestCustomerWealth/1672-RichestCustomerWealth.java
@@ -0,0 +1,17 @@
+// Last updated: 9/29/2025, 1:11:28 PM
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int sum = 0;
+        int ans = 0;
+        for(int i=0; i<accounts.length; i++){
+            for(int j=0; j<accounts[0].length; j++){
+                sum += accounts[i][j];
+
+                ans = Math.max(ans, sum);
+            
+            }
+            sum = 0;
+        }
+        return ans;
+    }
+}
