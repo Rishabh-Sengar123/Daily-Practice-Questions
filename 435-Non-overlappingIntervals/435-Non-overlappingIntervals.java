bd70cac4949d2364f2488fc012dad52a1ccda7f4
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-10-01 23:16:53 +0530

    File updated at 10/1/2025, 11:16:53 PM

--- 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
+++ 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
@@ -1,18 +1,19 @@
-// Last updated: 10/1/2025, 11:15:08 PM
+// Last updated: 10/1/2025, 11:16:53 PM
 import java.util.*;
 
 class Solution {
-    public int[][] reconstructQueue(int[][] people) {
-        Arrays.sort(people, (a, b) -> {
-            if (a[0] == b[0]) return a[1] - b[1];
-            return b[0] - a[0];
-        });
-
-        List<int[]> result = new ArrayList<>();
-        for (int[] person : people) {
-            result.add(person[1], person);
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0, right = people.length - 1;
+        int boats = 0;
+        
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            boats++;
         }
-
-        return result.toArray(new int[people.length][]);
+        return boats;
     }
 }
