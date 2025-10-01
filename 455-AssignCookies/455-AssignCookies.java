4ed2d0a891a3963f184a1c624d0254f4c1b37341
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-10-01 11:41:03 +0530

    File updated at 10/1/2025, 11:41:02 AM

--- 455-AssignCookies/455-AssignCookies.java
+++ 455-AssignCookies/455-AssignCookies.java
@@ -1,21 +1,11 @@
-// Last updated: 10/1/2025, 11:26:48 AM
+// Last updated: 10/1/2025, 11:41:02 AM
 class Solution {
-    public int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(g);
-        Arrays.sort(s);
-        int ans = 0;
-        int i = 0;
-        int j = 0;
-
-        while(i<g.length && j<s.length){
-            if(s[j]>=g[i]){
-                ans++;
-                i++;
-                j++;
-            }else{
-                j++;
-            }
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) return false; 
+            maxReach = Math.max(maxReach, i + nums[i]);
         }
-        return ans;
+        return true;
     }
 }
