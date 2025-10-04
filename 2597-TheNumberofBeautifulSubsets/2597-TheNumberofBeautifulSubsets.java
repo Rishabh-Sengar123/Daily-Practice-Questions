c72a412be95b9922b87f5817585645dd725e13c9
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-10-04 14:38:04 +0530

    Create 2597-TheNumberofBeautifulSubsets

---
+++ 2597-TheNumberofBeautifulSubsets/2597-TheNumberofBeautifulSubsets.java
@@ -0,0 +1,34 @@
+// Last updated: 10/4/2025, 2:38:02 PM
+import java.util.*;
+
+class Solution {
+    public int beautifulSubsets(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 0;
+
+        for (int mask = 1; mask < (1 << n); mask++) {
+            List<Integer> subset = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    subset.add(nums[i]);
+                }
+            }
+
+            boolean beautiful = true;
+            for (int i = 0; i < subset.size(); i++) {
+                for (int j = i + 1; j < subset.size(); j++) {
+                    if (Math.abs(subset.get(i) - subset.get(j)) == k) {
+                        beautiful = false;
+                        break;
+                    }
+                }
+                if (!beautiful) break;
+            }
+
+            if (beautiful) count++;
+        }
+
+        return count;
+    }
+}
