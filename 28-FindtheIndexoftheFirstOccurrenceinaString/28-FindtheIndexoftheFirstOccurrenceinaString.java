0e385bfb3dd0d47aff7bdce6565363147ca13189
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-10-05 18:24:17 +0530

    File updated at 10/5/2025, 6:24:16 PM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,19 +1,20 @@
-// Last updated: 10/5/2025, 6:16:57 PM
+// Last updated: 10/5/2025, 6:24:16 PM
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
         int n = capacity.length;
         int[] remaining = new int[n];
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             remaining[i] = capacity[i] - rocks[i];
         }
         Arrays.sort(remaining);
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (additionalRocks >= remaining[i]) {
-                additionalRocks -= remaining[i];
+
+        for(int i=0; i<n; i++){
+            if(additionalRocks >= remaining[i]){
                 count++;
-            } else {
-                break;
+                additionalRocks -= remaining[i];
+            }else{
+                continue;
             }
         }
         return count;
