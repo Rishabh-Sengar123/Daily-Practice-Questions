5615a2afb57bcb6707f6abab18db32b21163400e
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-10-06 10:46:54 +0530

    File updated at 10/6/2025, 10:46:53 AM

--- 2251-NumberofFlowersinFullBloom/2251-NumberofFlowersinFullBloom.java
+++ 2251-NumberofFlowersinFullBloom/2251-NumberofFlowersinFullBloom.java
@@ -1,52 +1,16 @@
-// Last updated: 10/5/2025, 4:09:04 PM
-import java.util.*;
-
+// Last updated: 10/6/2025, 10:46:53 AM
 class Solution {
-    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
-        int[] ans = new int[people.length];
-        int[] start = new int[flowers.length];
-        int[] end = new int[flowers.length];
-
-        for (int i = 0; i < flowers.length; i++) {
-            start[i] = flowers[i][0];
-            end[i] = flowers[i][1];
-        }
-
-        Arrays.sort(start);
-        Arrays.sort(end);
-
-        for (int i = 0; i < people.length; i++) {
-            int starter = first(start, people[i]);
-            int ended = second(end, people[i]);
-            ans[i] = starter - ended;
-        }
-
-        return ans;
-    }
-
-    public int first(int[] arr, int target) {
-        int left = 0, right = arr.length;
+    public int minPairSum(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        int max = Integer.MIN_VALUE;
+        Arrays.sort(nums);
         while(left < right){
-            int mid = left + (right-left)/2;
-            if(arr[mid]<=target){
-                left = mid+1;
-            }else{
-                right = mid;
-            }
+            int ans = nums[left] + nums[right];
+            left++;
+            right--;
+            max = Math.max(max, ans);
         }
-        return left;
-    }
-
-    public int second(int[] arr, int target) {
-        int left = 0, right = arr.length;
-        while(left < right){
-            int mid = left + (right-left)/2;
-            if(arr[mid]<target){
-                left = mid+1;
-            }else{
-                right = mid;
-            }
-        }
-        return left;
+        return max;
     }
 }
