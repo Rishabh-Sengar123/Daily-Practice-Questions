4ff4d4d17b5a1b001071fa48083f7474d060bb0a
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-10-11 22:37:27 +0530

    Create 1550-ThreeConsecutiveOdds

---
+++ 1550-ThreeConsecutiveOdds/1550-ThreeConsecutiveOdds.java
@@ -0,0 +1,19 @@
+// Last updated: 10/11/2025, 10:37:25 PM
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]%2 != 0){
+                count++;
+
+            }else{
+                count = 0;
+            }
+
+            if(count == 3){
+                return true;
+            }
+        }
+        return false;
+    }
+}
