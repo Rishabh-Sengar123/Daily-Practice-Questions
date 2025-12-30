11b74689d4bf8fe0c46b61da26eba143f282b87e
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-12-30 10:40:15 +0530

    File updated at 12/30/2025, 10:40:14 AM

--- 997-FindtheTownJudge/997-FindtheTownJudge.java
+++ 997-FindtheTownJudge/997-FindtheTownJudge.java
@@ -1,50 +1,21 @@
-// Last updated: 10/8/2025, 10:50:03 AM
-class Solution {
-    private static final int SIZE = 9;
-
-    public void solveSudoku(char[][] board) {
-        solve(board);
-    }
-
-    private boolean solve(char[][] board) {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (board[i][j] == '.') {
-                    for (char num = '1'; num <= '9'; num++) {
-                        if (isSafe(board, i, j, num)) {
-                            board[i][j] = num;
-
-                            if (solve(board)) {
-                                return true;
-                            }
-
-                            board[i][j] = '.';
-                        }
-                    }
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    private boolean isSafe(char[][] board, int row, int col, char num) {
-        
-        for (int i = 0; i < SIZE; i++) {
-            if (board[row][i] == num || board[i][col] == num)
-                return false;
-        }
-
-        
-        int startRow = row - row % 3;
-        int startCol = col - col % 3;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[startRow + i][startCol + j] == num)
-                    return false;
-            }
-        }
-
-        return true;
-    }
-}
+// Last updated: 12/30/2025, 10:40:14 AM
+1public class Solution {
+2    public int findJudge(int n, int[][] trust) {
+3
+4        int[] trustIn = new int[n + 1];
+5        int[] trustOut = new int[n + 1];
+6
+7        for (int [] arr : trust){
+8            trustIn[arr[1]]++;
+9            trustOut[arr[0]]++;
+10        }
+11
+12        for(int i =1; i <= n; i++){
+13            if(trustIn[i] == n - 1 && trustOut[i] == 0){
+14                return i;
+15            }
+16        }
+17        
+18        return -1;
+19    }
+20}
