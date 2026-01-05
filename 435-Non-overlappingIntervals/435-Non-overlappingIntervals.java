c55484b18d45f43a32e165adf5f53067881a405b
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-05 12:52:25 +0530

    File updated at 1/5/2026, 12:52:23 PM

--- 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
+++ 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
@@ -1,19 +1,24 @@
-// Last updated: 10/1/2025, 11:16:53 PM
-import java.util.*;
-
-class Solution {
-    public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
-        int left = 0, right = people.length - 1;
-        int boats = 0;
-        
-        while (left <= right) {
-            if (people[left] + people[right] <= limit) {
-                left++;
-            }
-            right--;
-            boats++;
-        }
-        return boats;
-    }
-}
+// Last updated: 1/5/2026, 12:52:23 PM
+1import java.util.*;
+2
+3class Solution {
+4    public int eraseOverlapIntervals(int[][] intervals) {
+5        if (intervals.length == 0) return 0;
+6
+7        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+8
+9        int count = 0;
+10        int prevEnd = intervals[0][1];
+11
+12        for (int i = 1; i < intervals.length; i++) {
+13            if (intervals[i][0] < prevEnd) {
+14             
+15                count++;
+16            } else {
+17                prevEnd = intervals[i][1];
+18            }
+19        }
+20        return count;
+21    }
+22}
+23
