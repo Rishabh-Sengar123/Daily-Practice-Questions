640e31bed2c0b9ce36510432e1c8f22af613bd53
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-05 21:40:45 +0530

    File updated at 1/5/2026, 9:40:43 PM

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,17 +1,21 @@
-// Last updated: 1/5/2026, 9:23:09 PM
+// Last updated: 1/5/2026, 9:40:43 PM
 1class Solution {
 2    public int uniquePaths(int m, int n) {
-3
-4        int totalMoves = m + n - 2;
-5        int downMoves = m - 1;
-6
-7        long result = 1;
-8
-9        for (int i = 1; i <= downMoves; i++) {
-10            result = result * (totalMoves - downMoves + i) / i;
-11        }
-12
-13        return (int) result;
-14    }
-15}
-16
+3        int[][] dp = new int[m][n];
+4        for(int i=0; i<m; i++){
+5            dp[i][0] = 1;
+6        }
+7
+8        for(int i=0; i<n; i++){
+9            dp[0][i] = 1;
+10        }
+11
+12        for(int i=1; i<m; i++){
+13            for(int j=1; j<n; j++){
+14                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+15            }
+16        }
+17
+18        return dp[m-1][n-1];
+19    }
+20}
