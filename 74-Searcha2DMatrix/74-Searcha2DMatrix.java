5e2a10056a9f0c94421cbd79a47c78ba52401a84
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-05 15:46:59 +0530

    File updated at 1/5/2026, 3:46:57 PM

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.java
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.java
@@ -1,17 +1,15 @@
-// Last updated: 9/30/2025, 12:14:47 PM
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int row = 0;
-        int col = matrix[0].length-1;
-        while(row < matrix.length && col >=0){
-            if(matrix[row][col] == target){
-                return true;
-            }else if(target > matrix[row][col]){
-                row++;
-            }else{
-                col--;
-            }
-        }
-        return false;
-    }
-}
+// Last updated: 1/5/2026, 3:46:57 PM
+1class Solution {
+2    public boolean searchMatrix(int[][] matrix, int target) {
+3        int row = matrix.length;
+4        int c = matrix[0].length;
+5        for(int i=0; i<row; i++){
+6            for(int j=0; j<c; j++){
+7                if(matrix[i][j] == target){
+8                    return true;
+9                }
+10            }
+11        }
+12        return false;
+13    }
+14}
