5d4dedd2d50fd1e572335b4309507b6721ed1bf3
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-06 11:12:50 +0530

    File updated at 1/6/2026, 11:12:48 AM

--- 1137-N-thTribonacciNumber/1137-N-thTribonacciNumber.java
+++ 1137-N-thTribonacciNumber/1137-N-thTribonacciNumber.java
@@ -1,20 +1,20 @@
-// Last updated: 10/13/2025, 9:28:54 PM
-class Solution {
-    public int tribonacci(int n) {
-        if(n==0){
-            return 0;
-        }
-        if(n == 1 || n == 2){
-            return 1;
-        }
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-
-        for(int i=3; i<=n; i++){
-            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
-        }
-        return dp[n];
-    }
-}
+// Last updated: 1/6/2026, 11:12:48 AM
+1class Solution {
+2    public int tribonacci(int n) {
+3        if(n==0){
+4            return 0;
+5        }
+6        if(n == 1 || n == 2){
+7            return 1;
+8        }
+9        int[] dp = new int[n+1];
+10        dp[0] = 0;
+11        dp[1] = 1;
+12        dp[2] = 1;
+13
+14        for(int i=3; i<=n; i++){
+15            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+16        }
+17        return dp[n];
+18    }
+19}
