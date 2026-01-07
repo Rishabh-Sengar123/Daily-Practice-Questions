cfac8003c0c7d26c077eabfa29e3d2b0e20d2c1f
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-07 22:03:19 +0530

    File updated at 1/7/2026, 10:03:16 PM

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,23 +1,22 @@
-// Last updated: 10/9/2025, 12:20:41 AM
-class Solution {
-    public int fib(int n) {
-        return solve(n);
-    }
-
-    public static int solve(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        int a = 0;
-        int b = 1;
-        int c = 0;
-
-        for (int i = 2; i <= n; i++) {
-            c = a + b;
-            a = b;
-            b = c;
-        }
-
-        return c;
-    }
-}
+// Last updated: 1/7/2026, 10:03:16 PM
+1class Solution {
+2    int[] dp;
+3    public int fib(int n) {
+4        dp = new int[n+1];
+5        Arrays.fill(dp , -1);
+6        return recursion(n);
+7    }
+8    private int recursion(int n){
+9        //1. Ye hamara base case rahega
+10        if(n<=1){
+11            return n;
+12        }
+13        //2. Check that we solve this before or not
+14        if(dp[n] != -1){
+15            return dp[n];
+16        }
+17        //3. Yaha pe recursion call kr rahe hai
+18        dp[n] = recursion(n-1) + recursion(n-2);
+19        return dp[n];
+20    }
+21}
