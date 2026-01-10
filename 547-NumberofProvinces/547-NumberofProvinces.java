4d815db464fb1e01007a9654d171141e2c4a6bcc
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-10 21:35:12 +0530

    File updated at 1/10/2026, 9:35:08 PM

--- 547-NumberofProvinces/547-NumberofProvinces.java
+++ 547-NumberofProvinces/547-NumberofProvinces.java
@@ -1,25 +1,29 @@
-// Last updated: 12/30/2025, 10:40:39 AM
+// Last updated: 1/10/2026, 9:35:08 PM
 1class Solution {
 2    public int findCircleNum(int[][] isConnected) {
 3        int n = isConnected.length;
 4        boolean[] visited = new boolean[n];
 5        int provinces = 0;
-6
-7        for (int i = 0; i < n; i++){
-8            if (!visited[i]) {  
-9                dfs(isConnected, visited, i);
-10                provinces++;
-11            }
-12        }
-13        return provinces;
-14    }
-15
-16    public void dfs(int[][] isConnected, boolean[] visited, int i) {
-17        visited[i] = true;
-18        for (int j = 0; j < isConnected.length; j++) {
-19            if (isConnected[i][j] == 1 && !visited[j]) {
-20                dfs(isConnected, visited, j);
-21            }
-22        }
-23    }
-24}
+6        for(int i=0; i<n; i++){
+7            if(!visited[i]){
+8                bfs(i, isConnected, visited);
+9                provinces++;
+10            }
+11        }
+12        return provinces;
+13    }
+14    private void bfs(int start , int[][] isConnected, boolean[] visited){
+15        Queue<Integer> queue = new LinkedList<>();
+16        queue.add(start);
+17        while(!queue.isEmpty()){
+18            int node = queue.poll();
+19            visited[node] = true;
+20
+21            for(int nei = 0; nei < isConnected.length ; nei++){
+22                if(isConnected[node][nei] == 1 && !visited[nei]){
+23                    queue.add(nei);
+24                }
+25            }
+26        }
+27    }
+28}
