6482cab6986dcb91119996eb8dbb72d44d0c998a
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-14 12:34:07 +0530

    File updated at 1/14/2026, 12:34:02 PM

--- 260-SingleNumberIII/260-SingleNumberIII.java
+++ 260-SingleNumberIII/260-SingleNumberIII.java
@@ -1,20 +1,20 @@
-// Last updated: 10/11/2025, 10:31:43 PM
-class Solution {
-    public int[] singleNumber(int[] nums) {
-        int[] arr = new int[2];
-        Map<Integer, Integer> freq = new HashMap<>();
-        for(int num : nums){
-            freq.put(num, freq.getOrDefault(num, 0)+1);
-        }
-        int idx = 0;
-        for(int i=0; i<nums.length ; i++){
-            int count = freq.getOrDefault(nums[i], 0);
-            if(count == 1){
-                arr[idx] = nums[i];
-                idx++;
-                
-            }
-        }
-        return arr;
-    }
-}
+// Last updated: 1/14/2026, 12:34:02 PM
+1class Solution {
+2    public int[] singleNumber(int[] nums) {
+3        int[] arr = new int[2];
+4        Map<Integer, Integer> freq = new HashMap<>();
+5        for(int num : nums){
+6            freq.put(num, freq.getOrDefault(num, 0)+1);
+7        }
+8        int idx = 0;
+9        for(int i=0; i<nums.length ; i++){
+10            int count = freq.getOrDefault(nums[i], 0);
+11            if(count == 1){
+12                arr[idx] = nums[i];
+13                idx++;
+14                
+15            }
+16        }
+17        return arr;
+18    }
+19}
