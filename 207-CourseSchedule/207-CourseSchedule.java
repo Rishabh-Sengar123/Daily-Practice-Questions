cd369b3f66da6bef52ee4ee57c2584669b0b3745
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-19 10:39:09 +0530

    File updated at 1/19/2026, 10:39:04 AM

--- 207-CourseSchedule/207-CourseSchedule.java
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -1,47 +1,42 @@
-// Last updated: 12/31/2025, 11:05:16 AM
+// Last updated: 1/19/2026, 10:39:04 AM
 1class Solution {
 2    public boolean canFinish(int numCourses, int[][] prerequisites) {
 3        List<List<Integer>> graph = new ArrayList<>();
 4        for(int i=0; i<numCourses; i++){
 5            graph.add(new ArrayList<>());
-6
-7        }
-8
-9        int[] indegree = new int[numCourses];
-10
-11        for(int[] edge : prerequisites){
-12            int u = edge[0];
-13            int v = edge[1];
-14
-15            graph.get(u).add(v);
-16
-17            indegree[v]++;
-18
-19        }
-20
-21        Queue<Integer> queue = new LinkedList<>();
-22        for(int i=0; i<numCourses; i++){
-23            if(indegree[i] == 0){
-24                queue.add(i);
-25            }
-26        }
+6        }
+7        int[] indegree = new int[numCourses];
+8        for(int[] e : prerequisites){
+9            int u = e[0];
+10            int v = e[1];
+11
+12            graph.get(u).add(v);
+13
+14            indegree[v]++;
+15        }
+16        ArrayList<Integer> topo = new ArrayList<>();
+17        Queue<Integer> queue = new LinkedList<>();
+18        for(int i=0; i<numCourses; i++){
+19            if(indegree[i] == 0){
+20                queue.add(i);
+21
+22            }
+23        }
+24        while(!queue.isEmpty()){
+25            int node = queue.poll();
+26            topo.add(node);
 27
-28        ArrayList<Integer> topo = new ArrayList<>();
-29
-30        while(!queue.isEmpty()){
-31            int node = queue.poll();
-32            topo.add(node);
-33            for(int neighbor : graph.get(node)){
-34                indegree[neighbor]--;
-35                if(indegree[neighbor] == 0){
-36                    queue.add(neighbor);
-37                }
-38            }
-39        }
-40        if(topo.size() != numCourses){
-41            return false;
-42        }
-43        
-44        return true;
-45    }
-46}
+28            for(int neighbor : graph.get(node)){
+29                indegree[neighbor]--;
+30                if(indegree[neighbor] == 0){
+31                    queue.add(neighbor);
+32                }
+33            }
+34        }
+35        if(topo.size() != numCourses){
+36            return false;
+37        }
+38
+39        return true;
+40    }
+41}
