54608f56366cd62750c9f2e5635a4c3c933553d7
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-25 22:39:43 +0530

    File updated at 1/25/2026, 10:39:42 PM

--- 72-EditDistance/72-EditDistance.java
+++ 72-EditDistance/72-EditDistance.java
@@ -1,27 +1,35 @@
-// Last updated: 1/25/2026, 10:23:49 PM
+// Last updated: 1/25/2026, 10:39:42 PM
 1class Solution {
-2    public int minDistance(String word1, String word2) {
-3        int n = word1.length();
-4        int m = word2.length();
-5
-6        int[][] dp = new int[n+1][m+1];
-7        for(int i = 0; i <= n; i++) dp[i][0] = i; 
-8        for(int j = 0; j <= m; j++) dp[0][j] = j; 
-9
-10        for(int i = 1; i <= n; i++) {
-11            for(int j = 1; j <= m; j++) {
-12                if(word1.charAt(i-1) == word2.charAt(j-1)) {
-13                    dp[i][j] = dp[i-1][j-1]; 
-14                } else {
-15                    dp[i][j] = 1 + Math.min(
-16                        dp[i-1][j-1], // replace
-17                        Math.min(dp[i-1][j],   // delete
-18                                 dp[i][j-1])   // insert
-19                    );
-20                }
-21            }
-22        }
-23        return dp[n][m];
-24    }
-25}
-26
+2    int[][] dp;
+3    public int minDistance(String word1, String word2) {
+4        int n = word1.length();
+5        int m = word2.length();
+6        dp = new int[n][m];
+7        for(int i = 0; i < n; i++) {
+8            Arrays.fill(dp[i], -1);
+9        }
+10        return solve(word1 , word2, n-1, m-1);
+11    }
+12    private int solve(String word1, String word2, int i, int j){
+13        if(i<0){
+14            return j+1;
+15        }
+16        if(j<0){
+17            return i+1;
+18        }
+19        if(dp[i][j] != -1){
+20            return dp[i][j];
+21        }
+22        if(word1.charAt(i) == word2.charAt(j)){
+23            return dp[i][j] = solve(word1, word2, i-1, j-1);
+24        }
+25
+26        int replace = solve(word1, word2, i-1, j-1);
+27        int delete = solve(word1, word2, i-1, j);
+28        int insert = solve(word1, word2, i, j-1);
+29
+30        return dp[i][j] = 1 + Math.min(replace, Math.min(delete, insert));
+31
+32        
+33    }
+34}
