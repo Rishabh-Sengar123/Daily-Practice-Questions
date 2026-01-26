dc40c00db85b2ed3fa8d12ae45809aa42d6065b0
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-26 17:28:26 +0530

    File updated at 1/26/2026, 5:28:25 PM

--- 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
@@ -1,25 +1,35 @@
-// Last updated: 1/26/2026, 5:26:56 PM
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode(int x) { val = x; }
-8 * }
-9 */
-10class Solution {
-11    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-12        if (root == null || root == p || root == q) {
-13            return root;
-14        }
-15
-16        TreeNode left = lowestCommonAncestor(root.left, p, q);
-17        TreeNode right = lowestCommonAncestor(root.right, p, q);
-18
-19        if (left != null && right != null) {
-20            return root;
-21        }
-22        return (left != null) ? left : right;
-23    }
-24}
+// Last updated: 1/26/2026, 5:28:25 PM
+class Solution {
+
+        private final Deque<Integer> pathToP = new ArrayDeque<>();
+        private final Deque<Integer> pathToQ = new ArrayDeque<>();
+        private final Map<Integer, TreeNode> nodeByVal = new HashMap<>();
+
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            find(root, p, new ArrayList<>(), pathToP);
+            find(root, q, new ArrayList<>(), pathToQ);
+
+            var value = pathToP.pop();
+            while (!pathToQ.contains(value)) {
+                value = pathToP.pop();
+            }
+            return nodeByVal.get(value);
+        }
+
+        private void find(TreeNode current, TreeNode toFind, ArrayList<Integer> subResult, Deque<Integer> finalResult) {
+            if (current == null) {
+                return;
+            }
+
+            subResult.add(current.val);
+            nodeByVal.put(current.val, current);
+            if (current.val == toFind.val) {
+                finalResult.addAll(subResult.stream().toList().reversed());
+                return;
+            }
+
+            find(current.left, toFind, subResult, finalResult);
+            find(current.right, toFind, subResult, finalResult);
+            subResult.removeLast();
+        }
+    }
