941f67c1de4d4e75299f635681183d2e87881160
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-27 21:04:20 +0530

    File updated at 1/27/2026, 9:04:18 PM

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,22 +1,13 @@
-// Last updated: 1/7/2026, 10:03:16 PM
+// Last updated: 1/27/2026, 9:04:18 PM
 1class Solution {
-2    int[] dp;
-3    public int fib(int n) {
-4        dp = new int[n+1];
-5        Arrays.fill(dp , -1);
-6        return recursion(n);
-7    }
-8    private int recursion(int n){
-9        //1. Ye hamara base case rahega
-10        if(n<=1){
-11            return n;
-12        }
-13        //2. Check that we solve this before or not
-14        if(dp[n] != -1){
-15            return dp[n];
-16        }
-17        //3. Yaha pe recursion call kr rahe hai
-18        dp[n] = recursion(n-1) + recursion(n-2);
-19        return dp[n];
-20    }
-21}
+2    public int findNumbers(int[] nums) {
+3        int count = 0;
+4        for(int i=0; i<nums.length; i++){
+5            String s = String.valueOf(nums[i]);
+6            if(s.length()%2 == 0){
+7                count++;
+8            }
+9        }
+10        return count;
+11    }
+12}
