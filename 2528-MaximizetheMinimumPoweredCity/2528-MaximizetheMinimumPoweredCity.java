268c461eb5d0262bbca43ed26eeb81d3bde69b58
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-01-30 13:13:07 +0530

    File updated at 1/30/2026, 1:13:04 PM

--- 2528-MaximizetheMinimumPoweredCity/2528-MaximizetheMinimumPoweredCity.java
+++ 2528-MaximizetheMinimumPoweredCity/2528-MaximizetheMinimumPoweredCity.java
@@ -1,10 +1,17 @@
-// Last updated: 1/30/2026, 1:06:50 PM
+// Last updated: 1/30/2026, 1:13:04 PM
 1class Solution {
-2    public int findTheWinner(int n, int k) {
-3        int ans = 0;
-4        for(int i=2; i<=n; i++){
-5            ans = (ans + k)%i;
-6        }
-7        return ans + 1;
-8    }
-9}
+2    public int timeRequiredToBuy(int[] tickets, int k) {
+3        int time = 0;
+4        int target = tickets[k];
+5        
+6        for (int i = 0; i < tickets.length; i++) {
+7            if (i <= k) {
+8                time += Math.min(tickets[i], target);
+9            } else {
+10                time += Math.min(tickets[i], target - 1);
+11            }
+12        }
+13        
+14        return time;
+15    }
+16}
