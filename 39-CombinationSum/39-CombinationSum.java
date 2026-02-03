bd4770ffa932b5f6f84fc171054812fdfd887bcd
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-02-03 23:58:14 +0530

    File updated at 2/3/2026, 11:58:14 PM

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,23 +1,27 @@
-// Last updated: 8/3/2025, 10:43:29 AM
-class Solution {
-    public List<List<Integer>> combinationSum(int[] coin, int amount) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ll = new ArrayList<>();
-        Combination(coin, amount, ll, 0, ans);
-        return ans;
-    }
-    public static void Combination(int[] coin, int amount, List<Integer> ll, int idx, List<List<Integer>> ans) {
-        if (amount == 0) {
-            ans.add(new ArrayList<>(ll)); 
-            return;
-        }
-
-        for (int i = idx; i < coin.length; i++) {
-            if (amount >= coin[i]) {
-                ll.add(coin[i]); 
-                Combination(coin, amount - coin[i], ll, i, ans); 
-                ll.remove(ll.size() - 1); 
-            }
-        }
-    }
-}
+// Last updated: 2/3/2026, 11:58:14 PM
+1class Solution {
+2    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+3        List<List<Integer>> result = new ArrayList<>();
+4        List<Integer> current = new ArrayList<>();
+5        solve(candidates, target, 0, 0, result, current);
+6        return result;
+7    }
+8    private void solve(int[] candidates, int target, int idx, int sum, List<List<Integer>> result, List<Integer> current){
+9        if(sum == target){
+10            result.add(new ArrayList<>(current));
+11            return;
+12        }
+13        if(idx == candidates.length || sum > target){
+14            return ;
+15        }
+16        
+17        current.add(candidates[idx]);
+18        solve(candidates, target, idx, sum+candidates[idx], result, current);
+19        
+20        current.remove(current.size()-1);
+21
+22        solve(candidates, target, idx+1, sum, result, current);
+23        
+24    }
+25
+26}
