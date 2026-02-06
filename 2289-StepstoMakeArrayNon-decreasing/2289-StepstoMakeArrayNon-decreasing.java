f27b503db06eceae29070b0e8b80c60e40ba7d8d
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-02-06 10:13:28 +0530

    File updated at 2/6/2026, 10:13:27 AM

--- 2289-StepstoMakeArrayNon-decreasing/2289-StepstoMakeArrayNon-decreasing.java
+++ 2289-StepstoMakeArrayNon-decreasing/2289-StepstoMakeArrayNon-decreasing.java
@@ -1,15 +1,15 @@
-// Last updated: 2/4/2026, 6:56:17 PM
+// Last updated: 2/6/2026, 10:13:27 AM
 1class Solution {
 2        public int totalSteps(int[] A) {
 3        int n = A.length, res = 0, j = -1;
-4        int dp[] = new int[n], stack[] = new int[n];
-5        for (int i = n - 1; i >= 0; --i) {
-6            while (j >= 0 && A[i] > A[stack[j]]) {
+4        int[] dp = new int[n], stack = new int[n];
+5        for(int i=n-1; i>=0; i--){
+6            while(j>=0 && A[i]>A[stack[j]]){
 7                dp[i] = Math.max(++dp[i], dp[stack[j--]]);
 8                res = Math.max(res, dp[i]);
 9            }
 10            stack[++j] = i;
-11        }
+11        } 
 12        return res;
 13    }
 14}
