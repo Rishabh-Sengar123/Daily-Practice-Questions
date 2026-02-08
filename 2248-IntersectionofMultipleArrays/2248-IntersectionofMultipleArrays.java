aa60ce8ce79de87e7a994e89d708692bd84ac92e
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-02-08 23:04:27 +0530

    File updated at 2/8/2026, 11:04:26 PM

--- 2248-IntersectionofMultipleArrays/2248-IntersectionofMultipleArrays.java
+++ 2248-IntersectionofMultipleArrays/2248-IntersectionofMultipleArrays.java
@@ -1,30 +1,20 @@
-// Last updated: 10/11/2025, 10:07:05 PM
-class Solution {
-    public List<Integer> intersection(int[][] matrix) {
-        List<Integer> candidates = new ArrayList<>();
-        for (int num : matrix[0]) {
-            candidates.add(num);
-        }
-
-        // Step 2: Check each candidate in all other rows
-        Iterator<Integer> it = candidates.iterator();
-        while (it.hasNext()) {
-            int num = it.next();
-            for (int i = 1; i < matrix.length; i++) {
-                boolean found = false;
-                for (int val : matrix[i]) {
-                    if (val == num) {
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) {
-                    it.remove(); // remove if not present in this row
-                    break;
-                }
-            }
-        }
-        Collections.sort(candidates);
-        return candidates;
-    }
-}
+// Last updated: 2/8/2026, 11:04:26 PM
+1class Solution {
+2    public List<Integer> intersection(int[][] nums) {
+3        List<Integer> list = new ArrayList<>();
+4        HashMap<Integer, Integer> map = new HashMap<>();
+5        for(int i=0; i<nums.length; i++){
+6            for(int j=0; j<nums[i].length; j++){
+7                int num = nums[i][j];
+8                map.put(num, map.getOrDefault(num, 0)+1);
+9            }
+10        }
+11        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+12            if(entry.getValue() == nums.length){
+13                list.add(entry.getKey());
+14            }
+15        }
+16        Collections.sort(list);
+17        return list;
+18    }
+19}
