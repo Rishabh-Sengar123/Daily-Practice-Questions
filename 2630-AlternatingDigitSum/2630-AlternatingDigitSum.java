f575dd31aa524a43e0815978bbfb50aabcc4a90b
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-02-10 15:18:56 +0530

    Create 2630-AlternatingDigitSum

---
+++ 2630-AlternatingDigitSum/2630-AlternatingDigitSum.java
@@ -0,0 +1,18 @@
+// Last updated: 2/10/2026, 3:18:55 PM
+class Solution {
+    public int alternateDigitSum(int n) {
+        String s = String.valueOf(n);
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int digit = s.charAt(i) - '0';
+            if (i % 2 == 0) {
+                sum += digit; 
+            } else {
+                sum -= digit; 
+            }
+        }
+
+        return sum;
+    }
+}
