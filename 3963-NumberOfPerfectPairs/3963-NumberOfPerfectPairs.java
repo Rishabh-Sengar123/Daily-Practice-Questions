98bdcb365ae6eec3a7ca64d0fab59a27b0d2f5c3
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-02-10 15:17:28 +0530

    Create 3963-NumberOfPerfectPairs

---
+++ 3963-NumberOfPerfectPairs/3963-NumberOfPerfectPairs.java
@@ -0,0 +1,28 @@
+// Last updated: 2/10/2026, 3:17:27 PM
+import java.util.*;
+
+class Solution {
+    public long perfectPairs(int[] nums) {
+        int n = nums.length;
+        long[] arr = new long[n];
+        
+        for(int i = 0; i < n; i++){
+            arr[i] = Math.abs((long)nums[i]);
+        }
+        
+        Arrays.sort(arr);
+        
+        long count = 0;
+        int j = 0;
+        
+        for(int i = 0; i < n; i++){
+            while(j < n && arr[j] <= 2 * arr[i]){
+                j++;
+            }
+            // pairs (i, i+1 ... j-1)
+            count += (j - i - 1);
+        }
+        
+        return count;
+    }
+}
