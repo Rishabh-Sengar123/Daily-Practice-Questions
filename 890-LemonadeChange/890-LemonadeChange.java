8f4f6854872d91bcca5abe05aca5902a53eba1bf
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-02-10 15:21:53 +0530

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.java
@@ -0,0 +1,26 @@
+// Last updated: 2/10/2026, 3:21:51 PM
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int f=0;
+        int t=0;
+        for(int bill:bills){
+            if(bill==5){
+                f++;
+            }
+            else if(bill==10){
+                t++;
+                f--;
+            }
+            else if(t>0){
+                t--;
+                f--;
+            }else{
+                f-=3;
+            }
+        if(f<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
