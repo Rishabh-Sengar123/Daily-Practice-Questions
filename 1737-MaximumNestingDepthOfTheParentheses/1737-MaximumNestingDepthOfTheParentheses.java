25d57d742630aa3afea4fe6eac62a79ea6a084fe
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-02-10 15:20:32 +0530

    Create 1737-MaximumNestingDepthOfTheParentheses

---
+++ 1737-MaximumNestingDepthOfTheParentheses/1737-MaximumNestingDepthOfTheParentheses.java
@@ -0,0 +1,16 @@
+// Last updated: 2/10/2026, 3:20:31 PM
+class Solution {
+    public int maxDepth(String s) {
+        int count = 0;
+        int max = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                count++;
+                max = Math.max(count, max);
+            }else if(s.charAt(i) == ')'){
+                count--;
+            }
+        }
+        return max;
+    }
+}
