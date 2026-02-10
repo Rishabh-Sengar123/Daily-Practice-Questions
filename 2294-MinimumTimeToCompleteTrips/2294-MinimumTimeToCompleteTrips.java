59457c2f21e31f2df4852862320d97bf0c3af378
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-02-10 15:19:42 +0530

    Create 2294-MinimumTimeToCompleteTrips

---
+++ 2294-MinimumTimeToCompleteTrips/2294-MinimumTimeToCompleteTrips.java
@@ -0,0 +1,22 @@
+// Last updated: 2/10/2026, 3:19:40 PM
+class Solution {
+    public long minimumTime(int[] time, int totalTrips) {
+        long min_time = Arrays.stream(time).min().getAsInt();
+        long l = 1, m = 0, r = min_time * totalTrips;
+        long curr_trips = 0;
+        while (l <= r) {
+            m = (l + r) / 2;
+            curr_trips = 0;
+            for (int i = 0; i < time.length; i++) {
+                curr_trips += Math.floorDiv(m, time[i]);
+            }
+            if (curr_trips >= totalTrips) {
+                r = m - 1;
+            }
+            else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
