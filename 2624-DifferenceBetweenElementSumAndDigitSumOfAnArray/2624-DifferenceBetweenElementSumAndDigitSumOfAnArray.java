c365c0dcdb0674bde4982d37b5dce6b34de331a1
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-02-10 15:18:57 +0530

    Create 2624-DifferenceBetweenElementSumAndDigitSumOfAnArray

---
+++ 2624-DifferenceBetweenElementSumAndDigitSumOfAnArray/2624-DifferenceBetweenElementSumAndDigitSumOfAnArray.java
@@ -0,0 +1,23 @@
+// Last updated: 2/10/2026, 3:18:56 PM
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int digi = 0;
+        int sum = 0;
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+        }
+        for(int num = 0; num <nums.length; num++){
+            digi += rj(nums[num]);
+        }
+        return Math.abs(sum - digi);
+    }
+    private int rj(int num){
+        int rs = 0;
+        while(num>0){
+            int digit = num%10;
+            rs += digit; 
+            num/=10;
+        }
+        return rs;
+    }
+}
