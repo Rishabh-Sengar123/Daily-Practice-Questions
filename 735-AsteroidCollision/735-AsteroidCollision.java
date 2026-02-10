1017645803e34bf30f2f8a842d3c233222584aa3
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-02-10 15:22:14 +0530

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.java
@@ -0,0 +1,32 @@
+// Last updated: 2/10/2026, 3:22:13 PM
+import java.util.*;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int a : asteroids) {
+            boolean destroyed = false;
+            while (!stack.isEmpty() && a < 0 && stack.peek() > 0) {
+                if (Math.abs(a) > stack.peek()) {
+                    stack.pop();
+                    continue;
+                } else if (Math.abs(a) == stack.peek()) {
+                    stack.pop();
+                }
+                destroyed = true;
+                break;
+            }
+            if (!destroyed) {
+                stack.push(a);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
