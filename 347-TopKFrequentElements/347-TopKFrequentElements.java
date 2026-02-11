26ac770e43af9cf57b845d1e13db8a3aea245e86
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-02-11 14:38:16 +0530

    File updated at 2/11/2026, 2:38:15 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,4 +1,4 @@
-// Last updated: 2/11/2026, 2:35:51 PM
+// Last updated: 2/11/2026, 2:38:15 PM
 1class Solution {
 2    public int[] topKFrequent(int[] nums, int k) {
 3        HashMap<Integer, Integer> map = new HashMap<>();
@@ -17,7 +17,7 @@
 16        for(int i=0; i<k; i++){
 17            list.add(pq.poll().getKey());
 18        }
-19        Collections.reverse(list);
+19        // Collections.reverse(list);
 20        int size = list.size();
 21        int[] arr = new int[size];
 22        for(int i=0; i<size; i++){
