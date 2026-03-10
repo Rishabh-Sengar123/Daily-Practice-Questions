c0093299e82bd185c3b245ea1b75283efe468ca4
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2026-03-10 10:49:30 +0530

    File updated at 3/10/2026, 10:49:31 AM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,25 +1,25 @@
-// Last updated: 8/3/2025, 10:43:25 AM
-class Solution {
-    public int trap(int[] height) {
-        return traping(height);
-    }
-    public static int traping(int[] arr){
-        int n = arr.length;
-        int[] left = new int[n];
-        left[0] = arr[0];
-        for(int i = 1 ; i< arr.length; i++){
-            left[i] = Math.max(left[i-1], arr[i]);
-        }
-        int[] right = new int[n];
-        right[n-1] = arr[n-1];
-        for(int i = n-2; i >=0; i--){
-            right[i] = Math.max(right[i+1], arr[i]);
-        }
-        int sum = 0;
-        for(int i= 0; i<right.length; i++){
-            sum =sum +  Math.min(left[i] , right[i]) - arr[i];
-        }
-        return sum;
-    }
-    
-}
+// Last updated: 3/10/2026, 10:49:31 AM
+1class Solution {
+2    public int trap(int[] height) {
+3        return traping(height);
+4    }
+5    public static int traping(int[] arr){
+6        int n = arr.length;
+7        int[] left = new int[n];
+8        left[0]=arr[0];
+9        for(int i=1; i<n; i++){
+10            left[i] = Math.max(left[i-1], arr[i]);
+11        }
+12
+13        int[] right = new int[n];
+14        right[n-1]=arr[n-1];
+15        for(int i=n-2; i>=0; i--){
+16            right[i] = Math.max(right[i+1], arr[i]);
+17        }
+18        int sum = 0;
+19        for(int i=0; i<n; i++){
+20            sum += Math.min(left[i], right[i])-arr[i];
+21        }
+22        return sum;
+23    }
+24}
